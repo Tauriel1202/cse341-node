972ea7a6898a379f9b52e21b1fc4c072d12ce0cf
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-09-17 04:05:57 -0400

    thens + catches

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,26 +2,29 @@ const { MongoClient, ObjectId } = require("mongodb");
 
 console.log("controllers");
 
-async function single (req, res, next) {
-  // if('query' in req){
-    const uri = process.env.MONGO_URI.replace("cse341-node", "contacts");
-    const client = new MongoClient(uri);
-    const dbo = client.db("cse341-node");
-    const all = await dbo.collection("contacts").findOne({_id: new ObjectId(req.params.id)})
-    console.log('💩',all)
-    res.send(all)
-  // }
-  next()
+async function single(req, res, next) {
+  const uri = process.env.MONGO_URI.replace("cse341-node", "contacts");
+  const client = new MongoClient(uri);
+  const dbo = client.db("cse341-node");
+  const all = await dbo
+    .collection("contacts")
+    .findOne({ _id: new ObjectId(req.params.id) })
+    .then((all) => {
+      console.log("💩", all);
+      res.send(all);
+      next();
+    }).catch(e => {console.log(e)})
 }
 
 async function all(req, res) {
-  
   const uri = process.env.MONGO_URI.replace("cse341-node", "contacts");
   const client = new MongoClient(uri);
   const dbo = client.db("cse341-node");
   const all = await dbo.collection("contacts").find().toArray()
-  console.log(all)
-  res.send(all)
+  .then((all) => {
+  console.log(all);
+  res.send(all);
+}).catch(e => {console.log(e)})
 }
 
 module.exports = { single, all };
